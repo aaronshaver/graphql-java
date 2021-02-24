8e9a7ce79bd171f8a568fdc694b96ab32d9e80ba
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-02-24 15:44:28 -0800

    initial local commit

---
+++ src/main/java/com/aaronshaver/simplegraphql/SimplegraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.aaronshaver.simplegraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplegraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplegraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaronshaver/simplegraphql/SimplegraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronshaver.simplegraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplegraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
